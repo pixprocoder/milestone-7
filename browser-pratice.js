42932f7c5dc15842e56fea3cc9e4c8d742fb044e
Samsul kobir <89704576+skpixels@users.noreply.github.com>
2022-12-29 22:47:43 +0600

    module 2 notes added

---
+++ browser-pratice.js
@@ -0,0 +1,22 @@
+/**
+ * 1. show output after 3.5 second
+ * 2. take a number using prompt. add 200. then alert the result.
+ * 3. do you want to see href? if ok. then console.log(href)
+ * 4. purpose of cookies 5-7 line write in English on your own word.
+ * 5. 3 differences between local storage and session storage.
+ * ------------------
+ *      HOMEWORK
+ * ------------------
+ * 6. how javaScript run 5-7 line write in English on your own word.
+ * 7. list of things that are asynchronous in js/browser.
+ * 8. What is event loop in javaScript and how does it work.
+ * -----------------
+ *      OPTIONAL
+ * -----------------
+ * 9. create simple website with 2 input fields and a button. add product name, price. by clicking  the button it will be added to the local storage. you should add multiple as well.
+ * 10. Display product on the website. if they exist on the local storage show them from there.
+ * ------------------
+ *         MUST
+ * ------------------
+ * 11. Go to stack overflow. create an account. spend min 30 minutes.
+ */
---
+++ note.js
@@ -0,0 +1,20 @@
+/**
+ * MODULE 1.
+ * ---------------------------
+ * what is http?
+ * http vs https
+ * 7 http layers
+ *  * 3 tyres--------- . client . server .database
+ * ----------------------------
+ * MODULE 2
+ * ----------------------------
+ * browser location property's
+ * browser history
+ * cookies
+ * sessionStorage vs localStorage.
+ * cookies vs sessionStorage vs localStorage
+ * web api article link (https://developer.mozilla.org/en-US/docs/Web/API)
+ * ---------------------------
+ * MODULE 3.
+ * ----------------------------
+ */
