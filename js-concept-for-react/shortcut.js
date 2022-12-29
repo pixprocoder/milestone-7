f74926918fd0437b625122e6a2763979951ab4b5
Samsul kobir <89704576+skpixels@users.noreply.github.com>
2022-12-30 02:41:44 +0600

    ternary operatior explored

---
+++ js-concept-for-react/shortcut.js
@@ -0,0 +1,6 @@
+// 1. (true) 'string', 3, true,{}, []
+// 2. (false) '', false, null, undefined
+// 3. (ternary operator)
+const money = 30;
+const result = money < 20 ? "eat drink" : "alu";
+console.log(result);
